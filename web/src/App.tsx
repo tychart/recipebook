e006017da8d4fdcb95a3ea39ded3d9dc157c29c9
Jordan Brockbank <132620463+JBrockbank@users.noreply.github.com>
2026-02-09 18:55:14 -0700

    Added Basic Layout
    Created basic pages and components and added some very very basic initial code.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Home from './pages/Home';
+import Cookbook from './pages/Cookbook';
+import Recipe from './pages/Recipe';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
   const [testApi, setTestApi] = useState<string>('loading...')
 
   useEffect(() => {
@@ -19,26 +20,13 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cookbook/:id" element={<Cookbook />} />
+          <Route path="/recipe/:id" element={<Recipe />} />
+        </Routes>
+      </BrowserRouter>
       <div>
         Message from the API: {testApi}
       </div>
