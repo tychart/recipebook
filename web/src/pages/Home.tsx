841c278c44a861c78e0bae7387f0cc32a1dd15de
Tyler Chartrand <tyler@thechartrands.com>
2026-02-09 22:08:53 -0700

    Added in test text to see if the rebuild was successful

---
+++ web/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <div>Home</div>
+      <div>Test to test if GH actions works1. (added by Tyler, feel free to remove after test is done)</div>
+    </div>
+  );
+}
