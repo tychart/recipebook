f27444bb0722e6919ef157716ce6b3236e22ada2
Elaina Hales <evhales@byu.edu>
2026-02-27 22:10:06 -0700

    update to add default items to DB

--- db/CreateDB.sql
+++ db/CreateDB.sql
@@ -56,7 +56,7 @@ CREATE TABLE Recipe (
 CREATE TABLE Ingredients (
   Ingredient_ID SERIAL PRIMARY KEY,
   Recipe_ID INTEGER NOT NULL REFERENCES Recipe(Recipe_ID),
-  Amount Integer NOT NULL,
+  Amount FLOAT NOT NULL,
   Unit TEXT NOT NULL,
   Name TEXT NOT NULL
 );
@@ -64,6 +64,20 @@ CREATE TABLE Ingredients (
 CREATE INDEX recipe_embedding_idx
 ON Recipe
 USING hnsw (embedding vector_cosine_ops);
+
+Insert into users (username, password, email) values ('User1', 'password', 'user1@gmail.com');
+
+Insert into cookbook (Book_Name, Owner_ID) values ('Favorites', 1);
+
+INSERT INTO recipe (recipe_name, instructions, servings, creator_id, book_id)
+VALUES ('Fluffy Buttermilk Pancakes', 
+    '1. Melt the 6 Tablespoons (85g) of butter first. Microwave or stovetop—either is fine. Set aside to slightly cool until step 3. You don''t want it piping hot.
+2. In a large bowl, preferably with a pour spout, whisk the flour, sugar, baking powder, baking soda, and salt together until combined. Set aside.
+3. In another large bowl, whisk the eggs, buttermilk, and vanilla extract together until combined. Whisk in the melted butter. Pour the wet ingredients into the dry ingredients and gently whisk to combine. Make sure there are no patches of dry flour at the bottom of the bowl. The batter is thick and a few lumps are fine. Set aside as you heat the stove.
+4. Heat a griddle or large skillet over medium heat. Coat generously with butter or nonstick spray. Once it''s hot, drop/pour a heaping 1/4 cup of batter on the griddle. Cook until the edges look set and you notice holes in the pancake''s surface around the border, about 2 minutes. Flip and cook the other side until cooked through, about 1-2 more minutes. Coat griddle/skillet with butter or nonstick spray, if needed, for each batch of pancakes.
+5. Keep pancakes warm in a preheated 200°F (93°C) oven until all pancakes are cooked. Serve pancakes immediately with your choice of toppings.
+6. Cover and store leftover pancakes in the refrigerator for up to 5 days.', 
+    5, 1, 1);
 
 -- run searches such as:
 -- SELECT Recipe_ID, Recipe_name
