c06052d84d5a82cdd211b2b296b0e715698c0f21
carleyandelin <carleyna@byu.edu>
2026-03-22 03:47:28 -0600

    Changed recipe card UI to be similar to cookbook card

--- web/src/components/cards/RecipeCard.tsx
+++ web/src/components/cards/RecipeCard.tsx
@@ -4,7 +4,7 @@ import type { Recipe } from "../../../types/types";
 const RecipeCard = ({ recipe }: { recipe: Recipe }) => (
   <Link
     to={`/recipe/${recipe.id}`}
-    className="flex flex-col justify-center items-center aspect-square rounded-xl shadow-md hover:shadow-xl transform hover:-translate-y-1 transition-all bg-red-100 p-4"
+    className="flex flex-col justify-center items-center aspect-[4/5] rounded-xl border border-black shadow-md hover:shadow-xl transition-all hover:-translate-y-1 hover:-rotate-[5deg] bg-red-100 p-4"
   >
     <h3 className="text-center font-semibold text-lg text-red-900">
       {recipe.name}
