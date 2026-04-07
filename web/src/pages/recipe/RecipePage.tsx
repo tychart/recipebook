8c576630ea88071b569067511b010e01530f143c
Ellie Clark <elliejeanclark@gmail.com>
2026-04-06 18:37:35 -0600

    initial link from recipe back to cookbook working.

--- web/src/pages/recipe/RecipePage.tsx
+++ web/src/pages/recipe/RecipePage.tsx
@@ -4,6 +4,8 @@ import Instructions from "../../components/recipe/Instructions";
 import IngredientList from "../../components/recipe/IngredientList";
 import Notes from "../../components/recipe/Notes";
 import type { Recipe } from "../../../types/types";
+import type { Cookbook } from "../../../types/types";
+import { getCookbook } from "../../api/cookbooks";
 import RecipeImage from "../../components/recipe/RecipeImage";
 import { getRecipe } from "../../api/recipes";
 import RecipeShareModal from "../../components/RecipeShareModal";
@@ -11,6 +13,7 @@ import RecipeShareModal from "../../components/RecipeShareModal";
 export default function RecipePage() {
   const { id } = useParams<{ id: string }>();
   const [recipe, setRecipe] = useState<Recipe | null>(null);
+  const [parentCookbook, setParentCookbook] = useState<Cookbook | null>(null);
   const [loading, setLoading] = useState(true);
   const [showShare, setShowShare] = useState(false);
   const [error, setError] = useState<string | null>(null);
@@ -24,6 +27,13 @@ export default function RecipePage() {
       try {
         const data = await getRecipe(Number(id));
         setRecipe(data);
+        try {
+          const parentCookbook = await getCookbook(data.cookbook_id);
+          setParentCookbook(parentCookbook);
+        } catch (err) {
+          console.error("Error fetching cookbook:", err);
+          setParentCookbook(null);
+        }
       } catch (err) {
         console.error("Error fetching recipe:", err);
         setError("Failed to load recipe. Please try again later.");
@@ -39,15 +49,24 @@ export default function RecipePage() {
   if (error) return <p>{error}</p>;
   if (!recipe) return <p>Recipe not found</p>;
 
-  // const tags = recipe.recipe_tags
-  //   ? recipe.recipe_tags.split(",").map((tag: string) => tag.trim())
-  //   : [];
-
   return (
     <div className="py-6 max-w-4xl mx-auto">
       {/* Header */}
       <div className="flex items-center justify-between mb-4">
-        <h1 className="text-3xl font-semibold">{recipe.name}</h1>
+        <div>
+          <h1 className="text-3xl font-semibold">{recipe.name}</h1>
+          {parentCookbook && (
+            <p className="text-sm text-gray-600 dark:text-black-300 mt-5">
+              In{" "}
+              <Link
+                to={`/cookbook/${parentCookbook.id}`}
+                className="font-medium"
+              >
+                {parentCookbook.name}
+              </Link>
+            </p>
+          )}
+        </div>
 
         <div className="flex items-center gap-10">
           {/* TODO: Disable editing button if user is not the creator of the recipe */}
